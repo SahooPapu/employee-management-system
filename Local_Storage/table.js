11c3d57290f8d09abf7e7089dc7d259389f21365
papu_sahoo <sahoopapu3450@gmail.com>
2023-02-27 19:25:49 +0530

    Form_using_LocalStorage

---
+++ Local_Storage/table.js
@@ -0,0 +1,19 @@
+
+console.log("chodiii");
+
+var table = document.getElementById("student_table");
+
+var row = table.insertRow(0);
+
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
+var cell5 = row.insertCell(4);
+
+
+cell1.innerHTML = "88521";
+cell2.innerHTML = "Papu Sahu";
+cell3.innerHTML = "MCA";
+cell4.innerHTML = "2-1-2001";
+cell5.innerHTML = "Gehiede Magia";
